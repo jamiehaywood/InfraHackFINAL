6b6ad22a75ed99fe451a753725fcc0099186e014
Jamie Haywood <jamie.haywood@gmail.com>
2019-07-12 12:29:03 +0100

    manually fixing the ghpages

--- src/views/Map.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="map">
-    <h1>Map of lift status on tfl network</h1>
-  </div>
-</template>
--- src/views/Planner.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="planner">
-    <h1>This is live interactive planner, yeah</h1>
-  </div>
-</template>
